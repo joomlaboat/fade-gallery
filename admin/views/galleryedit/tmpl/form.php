5862f91db2740425543947e044f5c129c2aca0ef
JoomlaBoat <ivankomlev@gmail.com>
2023-02-26 18:31:51 -0500

    http changed to https

--- admin/views/galleryedit/tmpl/form.php
+++ admin/views/galleryedit/tmpl/form.php
@@ -2,7 +2,7 @@
 /**
  * Fade Javascript Image Gallery Joomla! 2.5 Native Component
  * @author Ivan komlev <support@joomlaboat.com>
- * @link http://www.joomlaboat.com
+ * @link https://www.joomlaboat.com
  * @copyright Copyright (C) 2018-2020. All Rights Reserved
  * @license GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html
 */
